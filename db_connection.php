19ff1d84e07ad7bb25ff3720c30d675d119ca068
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2024-12-09 11:43:39 +0100

    Add database connection script for local MySQL setup
    - Added PHP script to establish a connection to the MySQL database using localhost with credentials.

---
+++ db_connection.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "127.0.0.1"; // Database host
+$username = "root";        // Database username
+$password = "1234";        // Database password
+$dbname = "mydb";          // Database name
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
